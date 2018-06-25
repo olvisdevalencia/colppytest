fb8f6093fd7c280840a8c63c9ec449a70164cbc4
Olvis Quintana <olvisdevalencia@gmail.com>
2018-06-25 03:30:24 -0300

    connecting db to heroku

--- config/database.php
+++ config/database.php
@@ -1,5 +1,12 @@
 <?php
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
 
     /*
@@ -51,14 +58,13 @@ return [
             'database' => database_path('database.sqlite'),
             'prefix'   => '',
         ],
-
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
+            'host'      => $host,//env('DB_HOST', 'localhost'),
             'port'      => env('DB_PORT', '3306'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
+            'database'  => $database,//env('DB_DATABASE', 'forge'),
+            'username'  => $username,//env('DB_USERNAME', 'forge'),
+            'password'  => $password,//env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
